ec874f3f3f14a0a074bb5617bc35b4a3875ffa99
Nicolo Contin <nicocontin@gmail.com>
2024-10-17 17:49:52 +0200

    simplify lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ncontin <ncontin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:06:52 by ncontin           #+#    #+#             */
-/*   Updated: 2024/10/17 13:38:25 by ncontin          ###   ########.fr       */
+/*   Updated: 2024/10/17 17:48:49 by ncontin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new_el = ft_lstnew((*f)(lst->content));
 		if (new_el == NULL)
 		{
-			(*del)(new_lst);
-			free(new_lst);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
 		}
 		ft_lstadd_back(&new_lst, new_el);
 		lst = lst->next;
