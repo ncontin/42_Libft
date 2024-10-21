b0c6c7368f628b61f7542eed102763647f1647dd
Nicolo Contin <nicocontin@gmail.com>
2024-10-21 15:24:08 +0200

    add main to lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ncontin <ncontin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:06:52 by ncontin           #+#    #+#             */
-/*   Updated: 2024/10/17 17:48:49 by ncontin          ###   ########.fr       */
+/*   Updated: 2024/10/21 15:16:38 by ncontin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*new_el;
 
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
 	new_lst = NULL;
 	while (lst != NULL)
 	{
@@ -31,3 +33,57 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_lst);
 }
+
+// #include <stdio.h>
+
+// void	*ft_toup(void *content)
+// {
+// 	char	*uc;
+// 	char	*new_content;
+// 	char	*result;
+
+// 	uc = (char *)content;
+// 	new_content = ft_strdup(uc);
+// 	result = new_content;
+// 	while (*new_content)
+// 	{
+// 		if (*new_content >= 'a' && *new_content <= 'z')
+// 		{
+// 			*new_content -= 32;
+// 		}
+// 		new_content++;
+// 	}
+// 	return (result);
+// }
+// int	main(void)
+// {
+// 	t_list	*list;
+// 	t_list	*node1;
+// 	t_list	*node2;
+// 	t_list	*node3;
+// 	t_list	*node4;
+// 	t_list	*new_list;
+// 	t_list	*og_list;
+
+// 	list = NULL;
+// 	node1 = ft_lstnew(ft_strdup("node1"));
+// 	node2 = ft_lstnew(ft_strdup("node2"));
+// 	node3 = ft_lstnew(ft_strdup("node3"));
+// 	node4 = ft_lstnew(ft_strdup("node4"));
+// 	ft_lstadd_front(&list, node4);
+// 	ft_lstadd_front(&list, node3);
+// 	ft_lstadd_front(&list, node2);
+// 	ft_lstadd_front(&list, node1);
+// 	og_list = list;
+// 	while (og_list)
+// 	{
+// 		printf("list: %s\n", (char *)og_list->content);
+// 		og_list = og_list->next;
+// 	}
+// 	new_list = ft_lstmap(list, ft_toup, free);
+// 	while (new_list)
+// 	{
+// 		printf("new_list: %s\n", (char *)new_list->content);
+// 		new_list = new_list->next;
+// 	}
+// }
